3d7aa2763dbc64f5b5b4381a2eeceb44425b5a74
ericgaspar <junk.eg@free.fr>
2021-01-06 22:19:16 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -26,166 +26,60 @@ extra_php_dependencies="php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-curl php
 # EXPERIMENTAL HELPERS
 #=================================================
 
-readonly YNH_DEFAULT_COMPOSER_VERSION=1.10.17
-# Declare the actual composer version to use.
-# A packager willing to use another version of composer can override the variable into its _common.sh.
-YNH_COMPOSER_VERSION=${YNH_COMPOSER_VERSION:-$YNH_DEFAULT_COMPOSER_VERSION}
-
-# Execute a command with Composer
+# Send an email to inform the administrator
 #
-# usage: ynh_composer_exec [--phpversion=phpversion] [--workdir=$final_path] --commands="commands"
-# | arg: -v, --phpversion - PHP version to use with composer
-# | arg: -w, --workdir - The directory from where the command will be executed. Default $final_path.
-# | arg: -c, --commands - Commands to execute.
-ynh_composer_exec () {
-	# Declare an array to define the options of this helper.
-	local legacy_args=vwc
-	declare -Ar args_array=( [v]=phpversion= [w]=workdir= [c]=commands= )
-	local phpversion
-	local workdir
-	local commands
-	# Manage arguments with getopts
-	ynh_handle_getopts_args "$@"
-	workdir="${workdir:-$final_path}"
-	phpversion="${phpversion:-$YNH_PHP_VERSION}"
+# usage: ynh_send_readme_to_admin app_message [recipients]
+# | arg: app_message - The message to send to the administrator.
+# | arg: recipients - The recipients of this email. Use spaces to separate multiples recipients. - default: root
+#	example: "root admin@domain"
+#	If you give the name of a YunoHost user, ynh_send_readme_to_admin will find its email adress for you
+#	example: "root admin@domain user1 user2"
+ynh_send_readme_to_admin() {
+	local app_message="${1:-...No specific information...}"
+	local recipients="${2:-root}"
 
-	COMPOSER_HOME="$workdir/.composer" \
-		php${phpversion} "$workdir/composer.phar" $commands \
-		-d "$workdir" --quiet --no-interaction
-}
+	# Retrieve the email of users
+	find_mails () {
+		local list_mails="$1"
+		local mail
+		local recipients=" "
+		# Read each mail in argument
+		for mail in $list_mails
+		do
+			# Keep root or a real email address as it is
+			if [ "$mail" = "root" ] || echo "$mail" | grep --quiet "@"
+			then
+				recipients="$recipients $mail"
+			else
+				# But replace an user name without a domain after by its email
+				if mail=$(ynh_user_get_info "$mail" "mail" 2> /dev/null)
+				then
+					recipients="$recipients $mail"
+				fi
+			fi
+		done
+		echo "$recipients"
+	}
+	recipients=$(find_mails "$recipients")
 
-# Install and initialize Composer in the given directory
-#
-# usage: ynh_install_composer [--phpversion=phpversion] [--workdir=$final_path] [--install_args="--optimize-autoloader"] [--composerversion=composerversion]
-# | arg: -v, --phpversion - PHP version to use with composer
-# | arg: -w, --workdir - The directory from where the command will be executed. Default $final_path.
-# | arg: -a, --install_args - Additional arguments provided to the composer install. Argument --no-dev already include
-# | arg: -c, --composerversion - Composer version to install
-ynh_install_composer () {
-	# Declare an array to define the options of this helper.
-	local legacy_args=vwa
-	declare -Ar args_array=( [v]=phpversion= [w]=workdir= [a]=install_args= [c]=composerversion=)
-	local phpversion
-	local workdir
-	local install_args
-	local composerversion
-	# Manage arguments with getopts
-	ynh_handle_getopts_args "$@"
-	workdir="${workdir:-$final_path}"
-	phpversion="${phpversion:-$YNH_PHP_VERSION}"
-	install_args="${install_args:-}"
-	composerversion="${composerversion:-$YNH_COMPOSER_VERSION}"
+	local mail_subject="☁️🆈🅽🅷☁️: \`$app\` has important message for you"
 
-	curl -sS https://getcomposer.org/installer \
-		| COMPOSER_HOME="$workdir/.composer" \
-		php${phpversion} -- --quiet --install-dir="$workdir" --version=$composerversion \
-		|| ynh_die "Unable to install Composer."
-
-	# update dependencies to create composer.lock
-	ynh_composer_exec --phpversion="${phpversion}" --workdir="$workdir" --commands="install --no-dev $install_args" \
-		|| ynh_die "Unable to update core dependencies with Composer."
-}
-
-
-#=================================================
-
-
-# Add swap
-#
-# usage: ynh_add_swap --size=SWAP in Mb
-# | arg: -s, --size= - Amount of SWAP to add in Mb.
-ynh_add_swap () {
-	# Declare an array to define the options of this helper.
-	declare -Ar args_array=( [s]=size= )
-	local size
-	# Manage arguments with getopts
-	ynh_handle_getopts_args "$@"
-
-	local swap_max_size=$(( $size * 1024 ))
-
-	local free_space=$(df --output=avail / | sed 1d)
-	# Because we don't want to fill the disk with a swap file, divide by 2 the available space.
-	local usable_space=$(( $free_space / 2 ))
-
- 	SD_CARD_CAN_SWAP=${SD_CARD_CAN_SWAP:-0}
-
-	# Swap on SD card only if it's is specified
-	if ynh_is_main_device_a_sd_card && [ "$SD_CARD_CAN_SWAP" == "0" ]
+	local mail_message="This is an automated message from your beloved YunoHost server.
+Specific information for the application $app.
+$app_message
+---
+Automatic diagnosis data from YunoHost
+$(yunohost tools diagnosis | grep -B 100 "services:" | sed '/services:/d')"
+	
+	# Define binary to use for mail command
+	if [ -e /usr/bin/bsd-mailx ]
 	then
-		ynh_print_warn --message="The main mountpoint of your system '/' is on an SD card, swap will not be added to prevent some damage of this one, but that can cause troubles for the app $app. If you still want activate the swap, you can relaunch the command preceded by 'SD_CARD_CAN_SWAP=1'"
-		return
-	fi
-
-	# Compare the available space with the size of the swap.
-	# And set a acceptable size from the request
-	if [ $usable_space -ge $swap_max_size ]
-	then
-		local swap_size=$swap_max_size
-	elif [ $usable_space -ge $(( $swap_max_size / 2 )) ]
-	then
-		local swap_size=$(( $swap_max_size / 2 ))
-	elif [ $usable_space -ge $(( $swap_max_size / 3 )) ]
-	then
-		local swap_size=$(( $swap_max_size / 3 ))
-	elif [ $usable_space -ge $(( $swap_max_size / 4 )) ]
-	then
-		local swap_size=$(( $swap_max_size / 4 ))
+		local mail_bin=/usr/bin/bsd-mailx
 	else
-		echo "Not enough space left for a swap file" >&2
-		local swap_size=0
+		local mail_bin=/usr/bin/mail.mailutils
 	fi
 
-	# If there's enough space for a swap, and no existing swap here
-	if [ $swap_size -ne 0 ] && [ ! -e /swap_$app ]
-	then
-		# Create file
-		truncate -s 0 /swap_$app
-		
-		# set the No_COW attribute on the swapfile with chattr
-		chattr +C /swap_$app
-		
-		# Preallocate space for the swap file, fallocate may sometime not be used, use dd instead in this case
-		if ! fallocate -l ${swap_size}K /swap_$app
-		then
-			dd if=/dev/zero of=/swap_$app bs=1024 count=${swap_size}
-		fi
-		chmod 0600 /swap_$app
-		# Create the swap
-		mkswap /swap_$app
-		# And activate it
-		swapon /swap_$app
-		# Then add an entry in fstab to load this swap at each boot.
-		echo -e "/swap_$app swap swap defaults 0 0 #Swap added by $app" >> /etc/fstab
-	fi
+	# Send the email to the recipients
+	echo "$mail_message" | $mail_bin -a "Content-Type: text/plain; charset=UTF-8" -s "$mail_subject" "$recipients"
 }
-
-ynh_del_swap () {
-	# If there a swap at this place
-	if [ -e /swap_$app ]
-	then
-		# Clean the fstab
-		sed -i "/#Swap added by $app/d" /etc/fstab
-		# Desactive the swap file
-		swapoff /swap_$app
-		# And remove it
-		rm /swap_$app
-	fi
-}
-
-# Check if the device of the main mountpoint "/" is an SD card
-#
-# [internal]
-#
-# return 0 if it's an SD card, else 1
-ynh_is_main_device_a_sd_card () {
-	local main_device=$(lsblk --output PKNAME --noheadings $(findmnt / --nofsroot --uniq --output source --noheadings --first-only))
-
-	if echo $main_device | grep --quiet "mmc" && [ $(tail -n1 /sys/block/$main_device/queue/rotational) == "0" ]
-	then
-		return 0
-	else
-		return 1
-	fi
-}
-
 
